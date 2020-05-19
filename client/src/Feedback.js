463ff784fea535df69b71c251736508576c58f70
milescarberry <62004569+milescarberry@users.noreply.github.com>
2020-05-19 19:51:40 +0530

    commit

--- client/src/Feedback.js
+++ client/src/Feedback.js
@@ -94,9 +94,12 @@ const Feedback = () => {          // Feedback Function (Main Function)
 
 
     const uploadWidget = () => {
-        cloudinary.openUploadWidget({ cloud_name: REACT_APP_API_CLOUDINARY_CLOUD_NAME, upload_preset: REACT_APP_API_CLOUDINARY_UPLOAD_SECRET, tags: ['ebooks'] },
-            function (error, result) {
-                console.log(result);
+        window.cloudinary.openUploadWidget({ cloud_name: REACT_APP_API_CLOUDINARY_CLOUD_NAME, upload_preset: REACT_APP_API_CLOUDINARY_UPLOAD_SECRET, tags: ['ebooks'] },
+            function (error, result) {                 // the "result" variable is an array which stores the images
+                //console.log(result);
+
+                setValues({ ...values, uploadedFiles: result, uploadPhotosButtonText: `${result ? result.length : 0} Photos Uploaded` });   // using template string here
+
             });
     }
 
